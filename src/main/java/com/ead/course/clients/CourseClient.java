78b5223bff7d1baea1e6f12903e1a421352a8fdb
norma <normandojefferson@hotmail.com>
2025-09-15 14:47:44 -0300

    feat(synchronous-communication): adicionar variável de ambiente com o host de authuser

--- src/main/java/com/ead/course/clients/CourseClient.java
+++ src/main/java/com/ead/course/clients/CourseClient.java
@@ -5,6 +5,7 @@ import com.ead.course.dtos.UserDto;
 import com.ead.course.services.UtilsService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -28,12 +29,15 @@ public class CourseClient {
     @Autowired
     UtilsService utilsService;
 
+    @Value("${ead.api.url.authuser}")
+    String REQUEST_URL_AUTHUSER;
+
     public Page<UserDto> getAllUsersByCourse(UUID courseId, Pageable pageable) {
 
         List<UserDto> searchResult = null;
 
         // Construir a url
-        String url = utilsService.creatUrl(courseId, pageable);
+        String url = REQUEST_URL_AUTHUSER + utilsService.creatUrlGetAllUsersByCourse(courseId, pageable);
 
         log.debug("Request URL: {}", url);
         log.info("Request URL: {}", url);
